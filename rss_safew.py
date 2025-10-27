f59b74d61f0783666aee168e7f31edd9c01cbd36
tianya134 <wudif350@gmail.com>
2025-10-27 20:27:20 +0800

    Update rss_safew.py

--- rss_safew.py
+++ rss_safew.py
@@ -250,8 +250,8 @@ def build_caption(title, author, description, link):
     footer = """
 ✅论坛最新地址: 
 tyw29.cc  tyw30.cc tyw33.cc
-⚜️点击加入交流群: https://www.sfw.vc/tyw666
-⚜️天涯论坛（唯一联系）方式：
+✅点击加入交流群: https://www.sfw.vc/tyw666
+✅天涯论坛（唯一联系）方式：
 沈复： @tywcc
 沐泽： @ssss001
 怡怡： @yiyi3
