6a66ce4068a4ae934de21f86d48ca9a6e3bcb619
tianya134 <wudif350@gmail.com>
2025-10-27 20:19:01 +0800

    Update rss_safew.py

--- rss_safew.py
+++ rss_safew.py
@@ -248,14 +248,13 @@ def build_caption(title, author, description, link):
     if len(description) > MAX_DESCRIPTION_LENGTH:
         description = description[:MAX_DESCRIPTION_LENGTH] + "..."
     footer = """
-论坛最新地址:
-tyw29.cc  tyw30.cc  tyw33.cc
-点击前往福利通知群: https://www.sfw.vc/tyw777
-点击前往聊天群组: https://www.sfw.vc/tyw666
+论坛最新地址: 
+tyw29.cc  tyw30.cc tyw33.cc
+点击加入交流群: https://www.sfw.vc/tyw666
 天涯论坛（唯一联系）方式：
-沈复：＠tywcc
-沐泽：＠ssss001
-怡怡：＠yiyi3
+沈复： @tywcc
+沐泽： @ssss001
+怡怡： @yiyi3
     """.strip()
     return (
         f"{escape_markdown(title)}\n"
